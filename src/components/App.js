6b73328bb84c06c808400e0a6c386f305dfe2419
Bobby Bates <robertfbatesjr@gmail.com>
2022-06-04 21:10:11 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import Toggle from "./Toggle";
 
 function App() {
   return (
-    <div>
+    <>
       <h3>Toggle</h3>
       <Toggle />
-    </div>
+    </>
   );
 }
 
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  // State variable: isOn
+  // NOTE: Always use const for useState, this line isn't being mutated
+  const [isOn, setIsOn] = useState(false)
+
+  // NOTE: Toggle logic lives here:
+  const handleClick = () => setIsOn(isOn => !isOn)
+
+  const color = isOn ? 'red' : 'white'
+
+  return <button
+          style={{background: color}} onClick={handleClick}>
+            {isOn ? 'ON' : 'OFF'}
+          </button>;
 }
 
 export default Toggle;
